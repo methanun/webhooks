8f2bd5b71f7c54d89eacdebbc509173a97db7e07
S1998-HUP <siriphon.namphandung@gmail.com>
2022-02-16 23:01:39 +0700

    new file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum nisi animi at tenetur ea a rem repudiandae est libero. Maiores quae doloribus odio officia quibusdam totam voluptatibus id dolore!</h1>
+</body>
+</html>
